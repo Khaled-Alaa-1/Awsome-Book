1724ddf0c77f2c26e0470094a4d06d1b868120fe
José Fernández-Caminero <49853028+fdezcaminero@users.noreply.github.com>
2023-05-29 15:54:38 -0400

    Remove comments from JS file

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Get the saved data from localStorage or create an empty array
 const storeData = JSON.parse(localStorage.getItem('Added Books')) || [];
-// Add the books to the HTML
 function addBooks(books) {
-  // Use the map function to iterate over each book and create HTML elements
   return books.map((book, index) => `
        <p>${book.title}</p>
        <p>${book.author}</p>
@@ -11,7 +8,6 @@ function addBooks(books) {
      `).join('');
 }
 
-// Display the books on the webpage
 function displayBooks() {
   const container = document.querySelector('.container');
   const div = document.createElement('div');
@@ -20,35 +16,29 @@ function displayBooks() {
   container.innerHTML = '';
   container.appendChild(div);
 }
-// Save the data to localStorage
+
 function updateData() {
   localStorage.setItem('Added Books', JSON.stringify(storeData));
 }
 
-// Remove a book from the storeData array
 function removeBook(index) {
   storeData.splice(index, 1);
   updateData();
   displayBooks();
 }
 
-// Defining addNewBook function
 function addNewBook(title, author) {
-  // Create a new book object
   const newBook = {
     title,
     author,
   };
 
-  // Add the new book to the storeData array
   storeData.push(newBook);
 
-  // Update the data in localStorage and display the books
   updateData();
   displayBooks();
 }
 
-// Add an event listener to the remove button
 const container = document.querySelector('.container');
 container.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove')) {
@@ -57,7 +47,6 @@ container.addEventListener('click', (event) => {
   }
 });
 
-// Add an event listener to the form submit button
 const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -67,5 +56,4 @@ form.addEventListener('submit', (event) => {
   form.reset();
 });
 
-// Display the books when the page loads
 displayBooks();
